c5101925d86e22728a3c135ae1d883b110af58c4
Mark Ryk <marco.look@outlook.com>
2021-04-01 01:34:52 -0300

    adicionando hello world em java

---
+++ index/src/main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class main {
+	public static void main (String[] args) throws IOException {
+		System.out.println("Hello world!");
+		//Coloca esses arquivos na pasta eclipse-workspace do Eclipse
+	}
+}
